f07fe3e3d5dc77a914c7ddc97d9f2d945d737475
Andrespineda1020 <32283570+Andrespineda1020@users.noreply.github.com>
2019-02-13 16:12:29 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://glacial-retreat-62209.herokuapp.com/api/listings');
+      return $http.get('/api/listings');
     },
 
 	create: function(listing) {
-	  return $http.post('https://glacial-retreat-62209.herokuapp.com/api/listings', listing);
+	  return $http.post('/api/listings', listing);
     },
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-       return $http.delete('https://glacial-retreat-62209.herokuapp.com/api/listings', id);
+       return $http.delete('/api/listings', id);
     }
   };
 
