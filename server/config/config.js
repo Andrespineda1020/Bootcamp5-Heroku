0fb9553fdbbef387e2a93376262c60dc8a960b22
Andrespineda1020 <32283570+Andrespineda1020@users.noreply.github.com>
2019-02-13 16:08:51 -0500

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ module.exports = {
   db: {
     uri: 'mongodb://andrespineda1020:database20@ds161495.mlab.com:61495/listingsdatabase', //place the URI of your mongo database here.
   },
-  port: 8080
+    port: process.env.PORT || 8080
 };
